ba932d78ad2cbf853ebc8c62e5c00ff4cf64e539
git-volt.software <info@volt.software>
2022-11-24 20:57:50 -0500

    added seperate repos

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string name = "Logan";
+            int age = 34;
+            char firstInitial = 'L';
+            bool areYouMarried = true;
+            double moneyInYourPocket = 10.50;
+            decimal monetaryCalculations = 1234567.1098m;
+
+            Console.WriteLine($"Hello my name is {name} (first inital is {firstInitial}) and I am {age} years old. It is {areYouMarried} that I am married. I currently have {moneyInYourPocket} in my pocket and hope to have {monetaryCalculations} in the bank one day.");
+
         }
     }
 }
